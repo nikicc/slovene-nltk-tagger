6cc72a3766f520052ac7e5bfd4d4cbc83c7d3cb9
Nejc Banic <banic.nejc@gmail.com>
2011-12-15 18:48:44 +0100

    Transforming words and characters into UNICODE UTF-8 encoding.

--- testOutput.py
+++ testOutput.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*
 #MUltext-east
+
 from nltk import PunktWordTokenizer
 
 import nltk
@@ -12,11 +13,7 @@ unicode_text = u'Tistega večera sem preveč popil, zgodilo se je mesec dni po t
 unicode_text = unicode_text.encode('utf-8')
 tokens = PunktWordTokenizer().tokenize(unicode_text)
 text = nltk.Text(tokens)
-L= tagger.tag(text)
-#REZULTAT = [('Tistega', 'PPNMER'), ('ve\xc4\x8dera', 'SOMER'), ('sem', 'GP-SPE-N'), ('preve\xc4\x8d', 'RSN'), ('popil', 'GGDD-EM'), (',', ','), ('zgodilo', 'GGDD-ES'), ('se', 'ZP------K'), ('je', 'GP-STE-N'), ('mesec', 'SOMETN'), ('dni', 'SOMMR'), ('po', 'DM'), ('tem', 'ZK-MEM'), (',', ','), ('ko', 'VD'), ('sem', 'GP-SPE-N'), ('izvedel', 'GGDD-EM'), (',', ','), ('da', 'VD'), ('me', 'ZOP-ET--K'), ('\xc5\xbeena', 'SOZEI'), ('vara', 'PPNZEI'), ('.', '.')]
-#print REZULTAT
+result = tagger.tag(text)
 
-#b = [()]
-b = [(i[0].decode("utf-8").encode('cp1250'),i[1]) for i in L]
-for i in b:
-    print "("+i[0]+" : "+i[1]+")"
+uniResult = [(unicode(pair[0],"utf-8"), pair[1]) for pair in result]
+
