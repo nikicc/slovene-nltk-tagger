73f76bc99cd4199ee191f495fe9f6333b002920d
Nejc Banic <banic.nejc@gmail.com>
2011-12-15 11:35:00 +0100

    New program for reformating corpus jos1M.

--- preFormatCorpus.py
+++ preFormatCorpus.py
@@ -5,22 +5,6 @@ Created on 10. dec. 2011
 '''
 import sys
 import xml.dom.minidom
-'''
-if len(sys.argv) != 3:
-    print "TWO ARGUMENTS NEEDED!"
-    print "    First is the path for input file"
-    print "    Second is the paht for output file"
-    sys.exit()
-
-IN = sys.argv[1]
-OUT = sys.argv[2]
-'''
-
-IN = "C:/Users/banic/Desktop/jos100kv2_0.xml"
-OUT ="C:/users/banic/Desktop/test"
-doc = xml.dom.minidom.parse(IN)
-f = open("C:/users/banic/Desktop/test", "w")
-
 
 # term parsing
 def parseTerm(childrenT):
@@ -37,22 +21,32 @@ def parseTerm(childrenT):
                 #print j.getAttribute("msd"), j.tagName, j.firstChild.data
                 f.write((j.firstChild.data +"/"+ str(j.getAttribute("msd"))).encode('utf8'))
 
-#for all sentences
-for sentances in doc.getElementsByTagName("s"):
-     for i in sentances.childNodes:
-        if i.nodeName != "#text":
-            if i.nodeName == "S":
-                f.write(" ")
-            elif(i.nodeName == "term"):
-                parseTerm(i.childNodes)
-            elif(i.nodeName == "c"):
-                #print "Character",i.tagName, i.firstChild.data
-                f.write((i.firstChild.data).encode('utf8'))
-                f.write("/")
-                f.write((i.firstChild.data).encode('utf8'))
-            elif (i.nodeName == "w"):
-                #print i.getAttribute("msd"), i.tagName, i.firstChild.data
-                #string =str(i.firstChild.data) +"/"+ i.getAttribute("msd")
-                f.write((i.firstChild.data +"/"+ str(i.getAttribute("msd"))).encode('utf8'))
-     f.write("\n")
+
+
+dataName = ["jos1M-01","jos1M-02","jos1M-03","jos1M-04",
+            "jos1M-05","jos1M-06","jos1M-07","jos1M-08","jos1M-09","jos1M-10"]
+f = open("C:/users/banic/Desktop/corpus.pos", "w")
+
+for i in dataName:
+    IN = "C:/Users/banic/Desktop/jos1M/"+i+".xml"
+    doc = xml.dom.minidom.parse(IN)
+
+    #for all sentences
+    for sentances in doc.getElementsByTagName("s"):
+         for i in sentances.childNodes:
+            if i.nodeName != "#text":
+                if i.nodeName == "S":
+                    f.write(" ")
+                elif(i.nodeName == "term"):
+                    parseTerm(i.childNodes)
+                elif(i.nodeName == "c"):
+                    #print "Character",i.tagName, i.firstChild.data
+                    f.write((i.firstChild.data).encode('utf8'))
+                    f.write("/")
+                    f.write((i.firstChild.data).encode('utf8'))
+                elif (i.nodeName == "w"):
+                    #print i.getAttribute("msd"), i.tagName, i.firstChild.data
+                    #string =str(i.firstChild.data) +"/"+ i.getAttribute("msd")
+                    f.write((i.firstChild.data +"/"+ str(i.getAttribute("msd"))).encode('utf8'))
+         f.write("\n")
 f.close()
