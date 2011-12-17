8dff57b2197044d4ba3d30828e305d8fe3cda5d2
Niko Colneric <niko.colneric@gmail.com>
2011-12-17 19:38:35 +0100

    Reorganization, renaming and some fixes

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 About
 ======
+In this project we will implement NLTK Trigram Tagger for Slovene language.
+##Usage
+Unitl this taggers are build into NLTK, you can download the taggers from folder **slovene_taggers/** and use them in NLTK. 
 
-In this project we will implement NLTK Tagger for Slovene language.
+The example, which shows how to use slovene taggers, is in file **example.py**
 
-Folders description
-------
-* __trainer/__ : the code forked from [https://github.com/japerk/nltk-trainer](https://github.com/japerk/nltk-trainer "nltk-trainer"). This trainer is used to train the Trigram tagger.
+##Folders and files description
+* _**trainer/**_ : the code forked from [https://github.com/japerk/nltk-trainer](https://github.com/japerk/nltk-trainer "nltk-trainer"). This trainer is used to train the Trigram tagger.
 
-* __jos100k/__ : Slovene corpus taken from [JOS project](http://nl.ijs.si/jos "Slovene corpus") with 100.000 tagged words.
+* _**jos100k/**_ : Slovene corpus taken from [JOS project](http://nl.ijs.si/jos "Slovene corpus") with 100.000 tagged words.
 
-* __jos1M/__ : Slovene corpus taken from [JOS project](http://nl.ijs.si/jos "Slovene corpus") with million tagged words.
+* _**jos1M/**_ : Slovene corpus taken from [JOS project](http://nl.ijs.si/jos "Slovene corpus") with million tagged words.
+
+* _**pos/jos1M.pos**_ : this file is used as an input for trainer program from _trainer/_
+
+* _**transformJOS.py**_ : the code for transforming all _.xml_ corpuses from _jos1M/_ into _jos1M.pos_.
+
+* _**trainJOS.py**_ : the code for calling the script from _trainer/_ which trains the Tagger on _pos/jos1M.pos_ file.
---
+++ transformJOS.py
@@ -0,0 +1,60 @@
+import sys
+import os
+import xml.dom.minidom
+
+out = "jos1M.pos"
+out = os.path.join("pos", out)
+
+dataName = ["jos1M-01.xml","jos1M-02.xml","jos1M-03.xml",
+	    "jos1M-04.xml","jos1M-05.xml","jos1M-06.xml",
+	    "jos1M-07.xml","jos1M-08.xml","jos1M-09.xml",
+	    "jos1M-10.xml"]
+
+f = open(out, "w")
+
+for i in dataName:
+    IN = "jos1M/"+i
+    doc = xml.dom.minidom.parse(IN)
+    print "Finished parsing: "+IN
+
+    #for all sentences
+    for sentances in doc.getElementsByTagName("s"):
+         for i in sentances.childNodes:
+            if i.nodeName != "#text":
+                if i.nodeName == "S":
+                    f.write(" ")
+                elif(i.nodeName == "term"):
+                    parseTerm(i.childNodes)
+                elif(i.nodeName == "c"):
+                    #print "Character",i.tagName, i.firstChild.data
+		    if i.firstChild.data in [',','.']:
+		        f.write(" ")
+                    f.write((i.firstChild.data).encode('utf8'))
+                    f.write("/")
+                    f.write((i.firstChild.data).encode('utf8'))
+                elif (i.nodeName == "w"):
+                    #print i.getAttribute("msd"), i.tagName, i.firstChild.data
+                    #string =str(i.firstChild.data) +"/"+ i.getAttribute("msd")
+                    f.write((i.firstChild.data +"/"+ str(i.getAttribute("msd"))).encode('utf8'))
+         f.write("\n")
+    print "Finished transforming: "+IN
+f.close()
+print "The transformed corpus is in file: "+out
+
+
+# term parsing
+def parseTerm(childrenT):
+     for j in childrenT:
+        if j.nodeName != "#text":
+            if j.nodeName == "S":
+                f.write(" ")
+            elif(j.nodeName == "c"):
+                #print "Character",j.tagName, j.firstChild.data
+		if i.firstChild.data in [',','.']:
+		    f.write(" ")
+                f.write((j.firstChild.data).encode('utf8'))
+                f.write("/")
+                f.write((j.firstChild.data).encode('utf8'))
+            elif (j.nodeName == "w"):
+                #print j.getAttribute("msd"), j.tagName, j.firstChild.data
+                f.write((j.firstChild.data +"/"+ str(j.getAttribute("msd"))).encode('utf8'))
