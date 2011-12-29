a58fd0b685f4a8bfccc1af09c9f768e3b8cd7f8e
Niko Colneric <niko.colneric@gmail.com>
2011-12-29 02:14:41 +0100

    Some work on the paper, modified generation script. The fraction should be almost 1 not almost 0 and the evaluation woks just fine

--- generateTaggers.sh
+++ generateTaggers.sh
@@ -1,44 +1,36 @@
 # For evaluating the taggers after construction remove "--no-eval" argument.
-# By defult this evaluate on the whole corpus, which might take a lot of time.
-# To evaluate tagger on only a fraction of a corpus use argument "--fraction FRACTION"
+# By defult this evaluate on the whole corpus.
+# To evaluate tagger on only a fraction of a corpus (which is NOT used for training)
+# use argument "--fraction FRACTION"
 
+#
+# GENERATION
+#
+
+date
 # TrigramTagger without evaluation
 python trainer/train_tagger.py --filename slovene_taggers/TrigramTagger.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --default="-Neznan-" --no-eval ./pos
-
+date
 # BrillTagger without evaluation
 python trainer/train_tagger.py --filename slovene_taggers/BrillTagger.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --default="-Neznan-" --brill --no-eval ./pos
-
+date
 # ClassifierBasedPOSTagger - NaiveBayes without evaluation
 python trainer/train_tagger.py --filename slovene_taggers/NaiveBayes.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --default="-Neznan-" --sequential='' --classifier="NaiveBayes" --no-eval ./pos
-
-# ClassifierBasedPOSTagger - Maxent without evaluation
-python trainer/train_tagger.py --filename slovene_taggers/Maxent.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --default="-Neznan-" --sequential='' --classifier="Maxent" --no-eval ./pos
-
-# ClassifierBasedPOSTagger - NaiveBayes, Maxent without evaluation
-python trainer/train_tagger.py --filename slovene_taggers/NaiveBayesMaxent.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --default="-Neznan-" --sequential='' --classifier=["NaiveBayes","Maxent"] --no-eval ./pos
-
-# ClassifierBasedPOSTagger - DecisionTree without evaluation
-python trainer/train_tagger.py --filename slovene_taggers/DecisionTree.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --default="-Neznan-" --sequential='' --classifier="DecisionTree" --no-eval ./pos
-
+date
 
 #
-# WITH EVALUATION (--fraction 0.01)
+# EVALUATION (--fraction 0.99)
+# use 99% for generating the tagger and 1% for evaluation 
+# this is only used for evaluation, for real generation look above
 #
 
+date
 # TrigramTagger without evaluation
-#python trainer/train_tagger.py --filename slovene_taggers/TrigramTagger.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --fraction 0.01 --default="-Neznan-" ./pos
-
+python trainer/train_tagger.py --filename tmp/TrigramTagger.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --fraction 0.99 --default="-Neznan-" ./pos
+date
 # BrillTagger without evaluation
-#python trainer/train_tagger.py --filename slovene_taggers/BrillTagger.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --fraction 0.01 --default="-Neznan-" --brill ./pos
-
+python trainer/train_tagger.py --filename tmp/BrillTagger.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --fraction 0.99 --default="-Neznan-" --brill ./pos
+date
 # ClassifierBasedPOSTagger - NaiveBayes without evaluation
-#python trainer/train_tagger.py --filename slovene_taggers/NaiveBayes.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --fraction 0.01 --default="-Neznan-" --sequential='' --classifier="NaiveBayes" ./pos
-
-# ClassifierBasedPOSTagger - Maxent without evaluation
-#python trainer/train_tagger.py --filename slovene_taggers/Maxent.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --fraction 0.01 --default="-Neznan-" --sequential='' --classifier="Maxent" ./pos
-
-# ClassifierBasedPOSTagger - NaiveBayes, Maxent without evaluation
-#python trainer/train_tagger.py --filename slovene_taggers/NaiveBayesMaxent.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --fraction 0.01 --default="-Neznan-" --sequential='' --classifier=["NaiveBayes","Maxent"] ./pos
-
-# ClassifierBasedPOSTagger - DecisionTree without evaluation
-#python trainer/train_tagger.py --filename slovene_taggers/DecisionTree.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --fraction 0.01 --default="-Neznan-" --sequential='' --classifier="DecisionTree" ./pos
+python trainer/train_tagger.py --filename tmp/NaiveBayes.pickle --reader nltk.corpus.reader.tagged.TaggedCorpusReader --fileids '.+\.pos' --fraction 0.99 --default="-Neznan-" --sequential='' --classifier="NaiveBayes" ./pos
+date
